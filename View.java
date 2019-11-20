a7e922fafe1bdf2f74e6ee7533c3268ed36a0413
KaristonVeluraz <kg97@interia.pl>
2019-11-20 11:50:09 +0100

    New view for cards list

--- View.java
+++ View.java
@@ -6,10 +6,28 @@ public class View {
     }
 
     public void printCardStat(Card card){
-        System.out.println("Unit name: " + card.getName());
-        System.out.println("Attack: " + card.getAttack());
-        System.out.println("Defense: " + card.getDefense());
-        System.out.println("Magic: " + card.getMagic());
+        System.out.println();
+        System.out.println("++++++++++++++++++++");
+        System.out.println("+                  +");
+        String[] stringsName = card.getName().split(" ");
+        for (int i  = 0; i < stringsName.length; i++ ){
+            if (i % 2 != 0 || i == stringsName.length-1) {
+                System.out.println("  " + stringsName[i].toUpperCase());
+            }
+            else{
+                System.out.print("  " + stringsName[i].toUpperCase());
+            }
+
+
+        }
+        System.out.println("+                  +");
+        System.out.println("+  Attack: " + card.getAttack());
+        System.out.println("+  Defense: " + card.getDefense());
+        System.out.println("+  Magic: " + card.getMagic() );
+        System.out.println("+                  +");
+        System.out.println("++++++++++++++++++++");
+
+
     }
 
 
